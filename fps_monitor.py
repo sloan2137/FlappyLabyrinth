a876b68fcb00ce18e9f1bafb3e46ef9be4008561
chupson <bratek.wojtek@gmail.com>
2023-05-29 16:55:19 +0200

    implemented fps monitor in stand-alone maze mini-game run

--- fps_monitor.py
+++ fps_monitor.py
@@ -1,6 +1,8 @@
 import pygame
 import time
 
+import draw_text
+
 UPDATE_EVERY_SECONDS = 1
 
 
@@ -9,13 +11,16 @@ class FPSMonitor:
         self.screen = screen
         self.last_update_time = time.time()
         self.frames_since_last_update = 0
-        self.fps = None
+        self.fps: int = 0
 
     def draw_fps(self):
-        pass
+        draw_text.draw_in_corner(self.screen, f"FPS: {self.fps}", draw_text.CORNERS.TOP_LEFT, (255, 255, 255),
+                                 "Arial", 12)
 
     def update(self):
         self.frames_since_last_update += 1
         if time.time() - self.last_update_time >= UPDATE_EVERY_SECONDS:
-            self.fps = self.frames_since_last_update / UPDATE_EVERY_SECONDS
+            self.fps = self.frames_since_last_update // UPDATE_EVERY_SECONDS
+            self.frames_since_last_update = 0
+            self.last_update_time = time.time()
         self.draw_fps()
