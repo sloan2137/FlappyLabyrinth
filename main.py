4dafda6c42d7ddcf852a36a8551c61ad06b6e8f4
sloan2137 <122121075+sloan2137@users.noreply.github.com>
2023-06-04 22:37:18 +0200

    add ground and make it move

--- main.py
+++ main.py
@@ -10,6 +10,21 @@ screen_height=720
 screen=pygame.display.set_mode((screen_width, screen_height))
 
 sky_image=pygame.image.load("assets1/background.png")
+ground_image=pygame.image.load("assets1/ground.png")
+
+scroll_speed=1
+
+class Ground(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=ground_image
+        self.rect=self.image.get_rect()
+        self.rect.x, self.rect.y=x, y
+    
+    def update(self):
+        self.rect.x-=scroll_speed
+        if self.rect.x<=-screen_width:
+            self.kill()
 
 def exit_game():
     for event in pygame.event.get():
@@ -18,7 +33,11 @@ def exit_game():
             exit()
 
 def main():
-    
+
+    x_ground, y_ground=0,520
+    ground=pygame.sprite.Group()
+    ground.add(Ground(x_ground, y_ground))
+
     run=True
     while run:
         
@@ -27,6 +46,13 @@ def main():
         screen.fill((0,0,0))
 
         screen.blit(sky_image, (0,0))
+
+        if len(ground)<=2:
+            ground.add(Ground(screen_width, y_ground))
+        
+        ground.draw(screen)
+
+        ground.update()
 
         clock.tick(60)
         pygame.display.update()
