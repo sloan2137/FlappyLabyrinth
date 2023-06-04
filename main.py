759b999b609259928b71410d9ba4861fdcd91b3c
sloan2137 <122121075+sloan2137@users.noreply.github.com>
2023-06-04 22:41:36 +0200

    add the bird and its animations

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ screen=pygame.display.set_mode((screen_width, screen_height))
 
 sky_image=pygame.image.load("assets1/background.png")
 ground_image=pygame.image.load("assets1/ground.png")
+bird_images=[pygame.image.load("assets1/bird_down.png"),
+             pygame.image.load("assets1/bird_mid.png"),
+             pygame.image.load("assets1/bird_up.png")]
 
 scroll_speed=1
 
@@ -32,11 +35,29 @@ def exit_game():
             pygame.quit()
             exit()
 
+bird_start_pos=(100,250)
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=bird_images[0]
+        self.rect=self.image.get_rect()
+        self.rect.center=bird_start_pos
+        self.image_index=0
+    
+    def update(self):
+        self.image_index+=1
+        if self.image_index>=30:
+            self.image_index=0
+        self.image=bird_images[self.image_index//10]
+
 def main():
 
     x_ground, y_ground=0,520
     ground=pygame.sprite.Group()
     ground.add(Ground(x_ground, y_ground))
+
+    bird=pygame.sprite.GroupSingle()
+    bird.add(Bird())
 
     run=True
     while run:
@@ -51,8 +72,11 @@ def main():
             ground.add(Ground(screen_width, y_ground))
         
         ground.draw(screen)
+        bird.draw(screen)
+
 
         ground.update()
+        bird.update()
 
         clock.tick(60)
         pygame.display.update()
