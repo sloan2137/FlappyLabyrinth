8a124c004b98c83cc6604030e6637269b8e63840
sloan2137 <122121075+sloan2137@users.noreply.github.com>
2023-06-10 20:46:01 +0200

    added a score, collisions and game over screen

--- main.py
+++ main.py
@@ -16,8 +16,66 @@ bird_images=[pygame.image.load("assets1/bird_down.png"),
              pygame.image.load("assets1/bird_up.png")]
 top_obstacle_image=pygame.image.load("assets1/pipe_top.png")
 bottom_obstacle_image=pygame.image.load("assets1/pipe_bottom.png")
+game_over_image=pygame.image.load("assets1/game_over.png")
 
-scroll_speed=1
+scroll_speed=2
+bird_start_pos=(100,250)
+score=0
+font=pygame.font.SysFont('Segoe', 26)
+
+class Bird(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=bird_images[0]
+        self.rect=self.image.get_rect()
+        self.rect.center=bird_start_pos
+        self.image_index=0
+        self.vel=0
+        self.jump=False
+        self.alive=True
+    
+    def update(self, user_input):
+        if self.alive:
+            self.image_index+=1
+        if self.image_index>=30:
+            self.image_index=0
+        self.image=bird_images[self.image_index//10]
+
+        self.vel+=0.5
+        if self.vel>7:
+            self.vel=7
+        if self.rect.y<500:
+            self.rect.y+=int(self.vel)
+        if self.vel==0:
+            self.jump=False
+
+        if user_input[pygame.K_SPACE] and not self.jump and self.rect.y>0 and self.alive:
+            self.jump=True
+            self.vel=-7
+
+class Obstacle(pygame.sprite.Sprite):
+    def __init__(self, x, y, image, obstacle_type):
+        pygame.sprite.Sprite.__init__(self)
+        self.image=image
+        self.rect=self.image.get_rect()
+        self.rect.x, self.rect.y=x, y
+        self.enter, self.exit, self.passed=False,False,False
+        self.obstacle_type=obstacle_type
+
+    def update(self):
+        self.rect.x-=scroll_speed
+        if self.rect.x<=-screen_width:
+            self.kill()
+        
+        global score
+        if self.obstacle_type=='bottom':
+            if bird_start_pos[0]>self.rect.topleft[0] and not self.passed:
+                self.enter=True
+            if bird_start_pos[0]>self.rect.topright[0] and not self.passed:
+                self.exit=True
+            if self.enter and self.exit and not self.passed:
+                self.passed=True
+                score+=1
 
 class Ground(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -37,64 +95,18 @@ def exit_game():
             pygame.quit()
             exit()
 
-bird_start_pos=(100,250)
-class Bird(pygame.sprite.Sprite):
-    def __init__(self):
-        pygame.sprite.Sprite.__init__(self)
-        self.image=bird_images[0]
-        self.rect=self.image.get_rect()
-        self.rect.center=bird_start_pos
-        self.image_index=0
-        self.vel=0
-        self.jump=False
-    
-    def update(self, user_input):
-        self.image_index+=1
-        if self.image_index>=30:
-            self.image_index=0
-        self.image=bird_images[self.image_index//10]
-
-        self.vel+=0.5
-        if self.vel>7:
-            self.vel=7
-        if self.rect.y<500:
-            self.rect.y+=int(self.vel)
-        if self.vel==0:
-            self.jump=False
-
-        if user_input[pygame.K_SPACE] and not self.jump and self.rect.y>0:
-            self.jump=True
-            self.vel=-7
-
-class Obstacle(pygame.sprite.Sprite):
-    def __init__(self, x, y, image):
-        pygame.sprite.Sprite.__init__(self)
-        self.image=image
-        self.rect=self.image.get_rect()
-        self.rect.x, self.rect.y=x, y
-
-    def update(self):
-        self.rect.x-=scroll_speed
-        if self.rect.x<=-screen_width:
-            self.kill()
-
-def exit_game():
-    for event in pygame.event.get():
-        if event.type==pygame.QUIT:
-            pygame.quit()
-            exit()
-
 def main():
-
-    x_ground, y_ground=0,520
-    ground=pygame.sprite.Group()
-    ground.add(Ground(x_ground, y_ground))
+    global score
 
     bird=pygame.sprite.GroupSingle()
     bird.add(Bird())
 
     obstacle_timer=0
     obstacles=pygame.sprite.Group()
+
+    x_ground, y_ground=0,520
+    ground=pygame.sprite.Group()
+    ground.add(Ground(x_ground, y_ground))
 
     run=True
     while run:
@@ -103,30 +115,45 @@ def main():
 
         screen.fill((0,0,0))
 
+        user_input=pygame.key.get_pressed()
+
         screen.blit(sky_image, (0,0))
 
         if len(ground)<=2:
             ground.add(Ground(screen_width, y_ground))
+        
+        obstacles.draw(screen)
+        ground.draw(screen)
+        bird.draw(screen)
 
-        user_input=pygame.key.get_pressed()
+        score_text=font.render('Score: ' + str(score), True, pygame.Color(255, 255, 255))
+        screen.blit(score_text, (20,20))
 
-        if obstacle_timer<=0:
+        if bird.sprite.alive:
+            ground.update()
+            obstacles.update()
+        bird.update(user_input)
+
+        collision_obstacles=pygame.sprite.spritecollide(bird.sprites()[0], obstacles, False)
+        collision_ground=pygame.sprite.spritecollide(bird.sprites()[0], ground, False)
+        if collision_obstacles or collision_ground:
+            bird.sprite.alive=False
+            if collision_ground:
+                screen.blit(game_over_image,(screen_width//2 - game_over_image.get_width()//2, 
+                                            screen_height//2 - game_over_image.get_height()//2))
+                if user_input[pygame.K_r]:
+                    score=0
+                    main()
+                    break
+
+        if obstacle_timer<=0 and bird.sprite.alive:
             x_top, x_bottom=550, 550
             y_top=random.randint(-600, -480)
             y_bottom=y_top+random.randint(90,130)+bottom_obstacle_image.get_height()
-            obstacles.add(Obstacle(x_top, y_top, top_obstacle_image))
-            obstacles.add(Obstacle(x_bottom, y_bottom, bottom_obstacle_image))
-            obstacle_timer=random.randint(225,260)
+            obstacles.add(Obstacle(x_top, y_top, top_obstacle_image, 'top'))
+            obstacles.add(Obstacle(x_bottom, y_bottom, bottom_obstacle_image, 'bottom'))
+            obstacle_timer=random.randint(110,150) #tu decyduje odleglosc
         obstacle_timer-=1
-        
-        ground.draw(screen)
-        bird.draw(screen)
-        obstacles.draw(screen)
-
-        ground.update()
-        bird.update(user_input)
-        obstacles.update()
-
 
         clock.tick(60)
         pygame.display.update()
